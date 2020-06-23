e8727f1b965e6a231496c04ce05016e9dde92e6a
gn-exia <djamelmiloudi3@gmail.com>
2020-06-23 19:10:26 +0200

    add images & smooth scroll

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,23 @@
-jQuery(document).ready(function() {
-	
+jQuery(document).ready(function () {
+
 	"use strict";
 	// Your custom js code goes here.
+	$(function () {
+		/**
+		* Smooth scrolling to page anchor on click
+		**/
+		$("a[href*='#']:not([href='#'])").click(function () {
+			if (
+				location.hostname == this.hostname
+				&& this.pathname.replace(/^\//, "") == location.pathname.replace(/^\//, "")
+			) {
+				var anchor = $(this.hash);
+				anchor = anchor.length ? anchor : $("[name=" + this.hash.slice(1) + "]");
+				if (anchor.length) {
+					$("html, body").animate({ scrollTop: anchor.offset().top }, 1500);
+				}
+			}
+		});
+	});
 
 });
